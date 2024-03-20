5c9e78840a17081e47a01086e9e59f053d70a35c
Sira <siraphop_2538@hotmail.com>
2024-03-20 19:06:16 +1100

    Implement hashMap class

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ class Node {
     this.head = head;
   }
 }
+
+class hashMap {
+  constructor() {
+    this.bucket = [];
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
+
+const exampleOne = new hashMap();
+
+console.log("Example Below");
+console.log(exampleOne.hash("Sara"));
