14068881f0e485056b9b000f470331db99dbaf41
Sira <siraphop_2538@hotmail.com>
2024-03-23 19:30:49 +1100

    now can store value inside bucket

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 class Node {
-  constructor(head) {
+  constructor(head, next = null) {
     this.head = head;
+    this.next = next;
   }
 }
 
 class hashMap {
   constructor() {
-    this.bucket = [];
+    this.buckets = new Array(16).fill(null);
   }
 
-  hash(key) {
+  hash(string) {
     let hashCode = 0;
 
     const primeNumber = 31;
-    for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    for (let i = 0; i < string.length; i++) {
+      hashCode = primeNumber * hashCode + string.charCodeAt(i);
     }
 
     return hashCode;
   }
+
+  set(key, value) {
+    let index = this.hash(key) % 16;
+
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bound");
+    }
+
+    console.log(index);
+
+    if (this.buckets[index] === null) {
+      this.buckets[index] = value;
+    }
+
+    console.log(this.buckets[index]);
+  }
 }
 
-const exampleOne = new hashMap();
+const hashBucket = new hashMap();
 
-console.log("Example Below");
-console.log(exampleOne.hash("Sara"));
+/* Set example*/
+
+hashBucket.set("Age", 32);
+hashBucket.set("Name", "Zin");
+hashBucket.set("Number", 5555);
+
+console.log(hashBucket.buckets);
